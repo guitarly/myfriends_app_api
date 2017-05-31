8778db350a84179908a84c156a9232a2ddf32c29
025609 <matthewnguyen@C1MRQ046HF4H.fios-router.home>
2017-05-30 21:58:23 -0400

    fixed correction

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -17,7 +17,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   end
 
   allow do
-    origins 'https://meandfriends.herokuapp.com', 'localhost:3050'
+    origins 'https://meandfriends.herokuapp.com', 'http://localhost:3050'
 
     resource '*',
       headers: :any,

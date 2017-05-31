c934386649794b05bd58db48e4eba97f774ceece
025609 <matthewnguyen@C1MRQ046HF4H.fios-router.home>
2017-05-31 12:27:00 -0400

    fixed cord file

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,8 +8,8 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
 
   allow do
-    origins 'https://meandfriends.herokuapp.com', 'localhost:3050'
-
+    # origins 'https://meandfriends.herokuapp.com', 'localhost:3050'
+origins 'https://meandfriends.herokuapp.com'
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
